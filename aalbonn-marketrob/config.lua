8bbfe591583b1da506360e39564f1a572bd3542c
aalbonn <96874392+aalbonn@users.noreply.github.com>
2022-06-15 21:25:50 +0300

    deleted config

--- aalbonn-marketrob/config.lua
+++
@@ -1,5 +0,0 @@
-Config = {}
-
-Config.SystemAvatar = 'https://cdn.discordapp.com/attachments/862095507174326292/862095549041737758/aalbonn.png'
-Config.UserAvatar = 'https://cdn.discordapp.com/attachments/862095507174326292/862095549041737758/aalbonn.png'
-Config.SystemName = 'aalbonn log'
